bceb0675f85fc88a96f6ca1013e04284ee6b67d0
kolawole opeyemi abdulmumeen <kopeyemi00@gmail.com>
2025-11-11 00:27:36 -0800

    update

---
+++ Agorithm/Test/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agorithm.Test
+{
+    internal class Class1
+    {
+        public class Kata
+        {
+            public static int ElapsedSeconds(DateTime startDate, DateTime endDate)
+            {
+                TimeSpan difference = endDate - startDate;
+                return (int)difference.TotalSeconds;
+            }
+        }
+    }
+}
---
+++ Agorithm/Test/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class Class2
+{
+    public class Kata
+    {
+        public static bool IsAnagram(string a, string b)
+        {
+            Char[] FirstArray = a.ToLower().ToArray();
+            Char[] SecondArray = b.ToLower().ToArray();
+
+            Array.Sort(FirstArray);
+            Array.Sort(SecondArray);
+
+            String Mecca = new String(FirstArray);
+            String Gadget = new String(SecondArray);
+
+
+            if (Mecca == Gadget)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
+
+
+
